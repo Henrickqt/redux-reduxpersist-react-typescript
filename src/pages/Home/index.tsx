758dca7ef7df249722ab537835bff4b8b42cb942
Henrickqt <pedro.msg@hotmail.com>
2020-09-19 21:26:06 -0300

    Cleaning the project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>
+        Inventore animi quo veritatis rerum quia architecto distinctio, <br/>
+        consequuntur beatae quis. Quae, error suscipit voluptatem neque <br/>
+        omnis temporibus adipisci labore vero eveniet!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
